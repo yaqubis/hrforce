b5de1df77d14c777ec32546048b2bd038abcf358
yaqubis <jorge.yaqub@gmail.com>
2026-09-09 11:17:13 -0300

    first commit

---
+++ src/main/java/br/com/hrforce/HrforceApplication.java
@@ -0,0 +1,13 @@
+package br.com.hrforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrforceApplication.class, args);
+	}
+
+}
